1be8b16616a14641596ae22aeba8a42ef21752fc
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-10-04 19:47:01 +0500

    Create Header and install bootsrap, redux dependencies

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,2 +1,13 @@
 import { NavLink } from 'react-router-dom';
 import logo from '../images/planet.png';
+
+function header() {
+  return (
+    <div className="d-flex align-items-center">
+      <img scr={logo} style={{display: 'inline-block'}}>
+      <h2>Space Travellers&apos; Hub</h2>  
+    </div>
+  )
+}
+
+export default header;
