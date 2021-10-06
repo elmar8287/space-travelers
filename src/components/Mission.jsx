d81f184cb17fa6509963b8f8c50922fd1aaf3a8f
elmar8287 <L-mar@inbox.ru>
2021-10-07 00:58:59 +0400

    Finish displaying missions

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,25 +1,58 @@
 import { PropTypes } from 'prop-types';
+import { useSelector } from 'react-redux';
 import './Mission.css';
 
-const Mission = ({ mission }) => (
-  <li key={mission.id}>
-    <div>
-      <div>
-        <h2>{mission.mission_name}</h2>
-        <p>{mission.description}</p>
-        <span>Active Memmber</span>
-        <button type="button">Join Mission</button>
-      </div>
-    </div>
-  </li>
-);
+// const Missions = ({ mission }) => (
+//   <li key={mission.id} className="list-unstyled">
+//     <div className="d-flex p-3 align-items-center">
+//       <div className="mission-description">
+//         <h2>{mission.mission_name}</h2>
+//         <p>{mission.description}</p>
+//         <button type="button" className="btn btn-primary">Join Mission</button>
+//       </div>
+//     </div>
+//   </li>
+// );
 
-export default Mission;
+const Missions = () => {
+  const allMissions = useSelector();
 
-Mission.propTypes = {
+  const missionComponents = allMissions.map((mission) => (
+    <tr key={mission.mission_id}>
+      <td><b>{mission.mission_name}</b></td>
+      <td>{mission.description}</td>
+      <td className="px-4 align-middle">
+        <span className="bg-secondary">NOT A MEMBER</span>
+      </td>
+      <td className="px-4 align-middle">
+        <button type="button">Join&nbsp;Mission</button>
+      </td>
+    </tr>
+  ));
+  return (
+    <container fluid>
+      <table className="my-3 table-bordered table-striped">
+        <thead>
+          <tr>
+            <td><b>Mission</b></td>
+            <td><b>Description</b></td>
+            <td><b>Status</b></td>
+            <td />
+          </tr>
+        </thead>
+        <tbody>
+          {missionComponents}
+        </tbody>
+      </table>
+    </container>
+  );
+};
+
+export default Missions;
+
+Missions.propTypes = {
   mission: PropTypes.shape({
     id: PropTypes.string.isRequired,
-    flickr_images: PropTypes.arrayOf().isRequired,
     mission_name: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
   }).isRequired,
--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,31 +1,34 @@
-const baseAPI = 'https://api.spacexdata.com/v3/';
-const FETCH_MISSIONS = 'space-travellers/missions/FETCH_MISSIONS';
-
+const apiEndpoint = 'https://api.spacexdata.com/v3/missions';
 const initialState = [];
-
-export const fetchMissions = () => async (dispatch) => {
-  const fetched = await fetch(`${baseAPI}missions`);
-  const list = await fetched.json();
-  const missions = [];
-  list.map((mission) => missions.push({
-    id: mission.id,
-    mission_name: mission.mission_name,
-    description: mission.description,
-  }));
-
-  dispatch({
-    type: FETCH_MISSIONS,
-    payload: missions,
+const ADD_MISSION = 'rockets/ADD_MISSION';
+const fetchAllMissions = async () => {
+  let res = await fetch(apiEndpoint);
+  res = await res.json();
+  return res;
+};
+const addMission = (mission) => ({
+  type: ADD_MISSION,
+  payload: mission,
+});
+export const fetchMissions = async (dispatch, getState) => {
+  if (getState().missionsReducer.length > 0) return;
+  const missions = await fetchAllMissions();
+  missions.forEach((mission) => {
+    const newMission = {
+      mission_id: mission.mission_id,
+      mission_name: mission.mission_name,
+      description: mission.description,
+    };
+    dispatch(addMission(newMission));
   });
 };
-
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case FETCH_MISSIONS: return [...action.payload];
+    case ADD_MISSION:
+      return [...state, action.payload];
     default:
       return state;
   }
 };
-
 export const missions = (state) => state.missionsReducer;
 export default reducer;
