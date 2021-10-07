10359f161118753c7bd7faab70fc4999bf32e724
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-10-07 16:26:16 +0500

    Import necessary files in profile.jsx and update PropTypes

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,16 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  Card,
+  Container,
+  Row,
+  Col,
+  ListGroup,
+  ListGroupItem,
+  Button,
+} from 'react-bootstrap';
+import { cancelReservation } from '../redux/rockets/rockets';
+import { missions } from '../redux/missions/missions';
 
 const Profile = () => (
   <div>
